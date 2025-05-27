6b104831c71b002edf4ed307385d78fcee8c0535
kiieeaa <kiyaadinda5@gmail.com>
2025-05-27 18:38:39 +0700

    vector dan pasien

--- Asosiasi.cpp/Asosiasi.cpp
+++ Asosiasi.cpp/Asosiasi.cpp
@@ -5,5 +5,10 @@ using namespace std;
 class doktor;
 
 class pasien{
-    public:
+    public:  //asosiasi
+     string nama;
+     vector<doktor*> daftar_doktor;
+     pasien(string pNama) : nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+     }
 }
