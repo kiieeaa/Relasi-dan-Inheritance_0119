f89e321a55f7e27c2360526bd3af611fdb0e7884
kiieeaa <kiyaadinda5@gmail.com>
2025-05-27 18:54:10 +0700

    auto digunakan dalam perulangan

--- Asosiasi.cpp/Asosiasi.cpp
+++ Asosiasi.cpp/Asosiasi.cpp
@@ -2,12 +2,12 @@
 #include <vector>
 using namespace std;
 
-class doktor;
+class dokter;
 
 class pasien{
     public:  //asosiasi
      string nama;
-     vector<doktor*> daftar_doktor;
+     vector<dokter*> daftar_doktor;
      pasien(string pNama) : nama(pNama) {
         cout << "Pasien \"" << nama << "\" ada\n";
      }
@@ -16,4 +16,43 @@ class pasien{
         cout <<"Pasien \"" << nama
         << "\" tidak ada\n";
      }
+     void tambahDokter(dokter*);
+     void cetakDokter();
+};
+
+class dokter {
+    public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) : nama (pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+
+    ~dokter() {
+        cout << "Dokter \"" << nama
+        <<"\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
+
+};
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+
+}
+
+
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menangani pasien \""
+    << this->nama << "\":\n";
+    //auto digunakan dalam perulangan
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+
+    cout << endl;
+
 }
