d69668d856be8b0c5aa1180cc96b90bbe05f0e3b
kiieeaa <kiyaadinda5@gmail.com>
2025-05-27 18:36:24 +0700

    Asosiasi

--- Agragasi.cpp/anak.h
+++ Agragasi.cpp/anak.h
@@ -1,11 +1,11 @@
 #ifndef ANAK_H
 #define ANAK_H
 
-class anak{
+class anak {
     public:
        string nama;
        anak(string pNama): nama(pNama) {
-          cout << "Anak \"" << nama << "\" ada\n"
+          cout << "Anak \"" << nama << "\" ada\n";
        }
 
        ~anak() {
---
+++ Asosiasi.cpp/Asosiasi.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class doktor;
+
+class pasien{
+    public:
+}
